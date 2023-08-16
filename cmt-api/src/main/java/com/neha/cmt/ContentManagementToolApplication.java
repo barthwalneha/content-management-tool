d38489f95022c22020aaf5ded8e657c5bb116531
barthwalneha <nbarthwal30@gmail.com>
2023-08-16 09:27:00 +0530

    cmi-api initaial setup

---
+++ cmt-api/src/main/java/com/neha/cmt/ContentManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.neha.cmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentManagementToolApplication.class, args);
+	}
+
+}
