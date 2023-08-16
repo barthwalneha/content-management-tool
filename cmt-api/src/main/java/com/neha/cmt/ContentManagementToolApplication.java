c68012ab3b2657ad3db2e218ad1c83d57a5af912
barthwalneha <nbarthwal30@gmail.com>
2023-08-16 09:30:53 +0530

    test from intelij

--- cmt-api/src/main/java/com/neha/cmt/ContentManagementToolApplication.java
+++ cmt-api/src/main/java/com/neha/cmt/ContentManagementToolApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ContentManagementToolApplication {
 
 	public static void main(String[] args) {
+		//Hello
 		SpringApplication.run(ContentManagementToolApplication.class, args);
 	}
 
